b73775c5fa0cbac0b67bc9ff2b95b5574f20c43b
kittyandrew <kitty@maudrew.dev>
2023-02-25 15:36:33 +0100

    remove unused fields (potentially fixes grafana v9 cmptblty - #3)

--- src/data.rs
+++ src/data.rs
@@ -15,25 +15,9 @@ pub struct Match {
 #[derive(Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
 pub struct Notification {
-    #[serde(alias = "dashboardId")]
-    pub dashboard_id: u64,
-    #[serde(alias = "evalMatches")]
-    pub eval_matches: Vec<Match>,
     // Note(andrew): 'message' field is not always present.
     pub message: Option<String>,
-    #[serde(alias = "orgId")]
-    pub org_id: u64,
-    #[serde(alias = "panelId")]
-    pub panel_id: u64,
-    #[serde(alias = "ruleId")]
-    pub rule_id: u64,
-    #[serde(alias = "ruleName")]
-    pub rule_name: String,
-    #[serde(alias = "ruleUrl")]
-    pub rule_url: String,
     pub state: String,
-    // TODO: Document type properly.
-    pub tags: Value,
     pub title: String,
 }
 
